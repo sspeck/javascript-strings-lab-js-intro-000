fb13408f2727d36f268ac53809fa7082069120ba
Sarah Speck <smspeck13@gmail.com>
2018-02-22 01:10:57 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -15,4 +15,4 @@ var greeting = "Hello, everybody!";
 var specialGuest = "Niel deGrasse Tyson"
 var greetSpecialGuest = `Hello, ${specialGuest}!`
 var topic = 'space';
-var conversation = 'Let\'s talk about ${topic}.'
+var conversation = `Let\'s talk about ${topic}.`

1cb6f51b5fa6feb4141d83a766a8e0573a8da46f
Sarah Speck <smspeck13@gmail.com>
2018-02-22 00:58:08 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -11,11 +11,11 @@ it('concatenates strings to greet a special guest in `greetSpecialGuest`', funct
 it('interpolates a string in `conversation`', function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
 })
-var greeting = "Hello, everbody!"
+var greeting = "Hello, everbody!";
 var specialGuest = "Niel deGrasse Tyson"
-var greetSpecialGuest = 'Hello' + specialGuest + '!'
-var topic = 'space'
-var conversation = `${topic}`
+var greetSpecialGuest = 'Hello' + specialGuest + '!';
+var topic = 'space';
+var conversation = `${topic}`;
 greeting = 'Hello, everybody!'
 greetSpecialGuest = `Hello ${specialGuest}!`
 conversation = "Let's talk about ${topic}."
